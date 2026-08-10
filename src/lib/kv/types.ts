3566e86b530eb15f6d62f607b22d6e86160c2ac6
t0ma5 <32952971+t0ma5@users.noreply.github.com>
2026-08-10 21:47:37 +0300

    Add files via upload

--- src/lib/kv/types.ts
+++ src/lib/kv/types.ts
@@ -1,23 +1,27 @@
-export enum SplitMode {
-  EVENLY = 'EVENLY',
-  BY_SHARES = 'BY_SHARES',
-  BY_PERCENTAGE = 'BY_PERCENTAGE',
-  BY_AMOUNT = 'BY_AMOUNT',
-}
+export const SplitMode = {
+  EVENLY: 'EVENLY',
+  BY_SHARES: 'BY_SHARES',
+  BY_PERCENTAGE: 'BY_PERCENTAGE',
+  BY_AMOUNT: 'BY_AMOUNT',
+} as const
+export type SplitMode = (typeof SplitMode)[keyof typeof SplitMode]
 
-export enum RecurrenceRule {
-  NONE = 'NONE',
-  DAILY = 'DAILY',
-  WEEKLY = 'WEEKLY',
-  MONTHLY = 'MONTHLY',
-}
+export const RecurrenceRule = {
+  NONE: 'NONE',
+  DAILY: 'DAILY',
+  WEEKLY: 'WEEKLY',
+  MONTHLY: 'MONTHLY',
+} as const
+export type RecurrenceRule = (typeof RecurrenceRule)[keyof typeof RecurrenceRule]
 
-export enum ActivityType {
-  UPDATE_GROUP = 'UPDATE_GROUP',
-  CREATE_EXPENSE = 'CREATE_EXPENSE',
-  UPDATE_EXPENSE = 'UPDATE_EXPENSE',
-  DELETE_EXPENSE = 'DELETE_EXPENSE',
-}
+export const ActivityType = {
+  UPDATE_GROUP: 'UPDATE_GROUP',
+  CREATE_EXPENSE: 'CREATE_EXPENSE',
+  UPDATE_EXPENSE: 'UPDATE_EXPENSE',
+  DELETE_EXPENSE: 'DELETE_EXPENSE',
+} as const
+export type ActivityType = (typeof ActivityType)[keyof typeof ActivityType]
+
 
 export type Participant = {
   id: string
